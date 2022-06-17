61d7d7ac6bb65b1230e1728396926959092cb6ff
Andr3sCid <a.cid04@ufromail.cl>
2022-06-17 16:25:15 -0400

    avance plantilla juego gato

---
+++ src/components/gato.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+
+    <h1>JUEGO DEL GATO</h1>
+    <h3>Comience el jugador X</h3>
+
+    <section id="zonaJuego">
+        <div id="zona1"></div>
+        <div id="zona2"></div>
+        <div id="zona3"></div>
+        <div id="zona4"></div>
+        <div id="zona5"></div>
+        <div id="zona6"></div>
+        <div id="zona7"></div>
+        <div id="zona8"></div>
+        <div id="zona9"></div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    body{
+        background-color: #042940;
+    }
+
+    h1, h3{
+        color: white;
+    }
+</style>
